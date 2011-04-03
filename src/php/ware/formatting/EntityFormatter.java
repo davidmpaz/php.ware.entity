e231d034e6745f6b8d9f0a4606103da31ee38b61
David Moises Paz Reyes <davidmpaz@gmail.com>
2011-04-03 14:05:30 -0400

    Add formating code
    Add more rules to formatting files.

--- src/php/ware/formatting/EntityFormatter.java
+++ src/php/ware/formatting/EntityFormatter.java
@@ -3,25 +3,44 @@
  */
 package php.ware.formatting;
 
+import java.util.List;
+
+import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
+import org.eclipse.xtext.util.Pair;
+
+import php.ware.services.EntityGrammarAccess;
 
 /**
  * This class contains custom formatting description.
  * 
  * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#formatting
- * on how and when to use it 
+ * on how and when to use it
  * 
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
 public class EntityFormatter extends AbstractDeclarativeFormatter {
-	
+
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-// It's usually a good idea to activate the following three statements.
-// They will add and preserve newlines around comments
-//		c.setLinewrap(0, 1, 2).before(getGrammarAccess().getSL_COMMENTRule());
-//		c.setLinewrap(0, 1, 2).before(getGrammarAccess().getML_COMMENTRule());
-//		c.setLinewrap(0, 1, 1).after(getGrammarAccess().getML_COMMENTRule());
+
+		EntityGrammarAccess f = (EntityGrammarAccess)getGrammarAccess();
+		// It's usually a good idea to activate the following three statements.
+		// They will add and preserve newlines around comments
+		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
+		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
+		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
+
+		c.setLinewrap(1, 2, 3).around(f.getAbstractElementRule());
+		c.setLinewrap(1, 2, 3).around(f.getPackageDeclarationRule());
+		c.setLinewrap(1, 1, 2).around(f.getFeatureRule());
+		c.setNoSpace().before(f.getTypeRefAccess().getMultiLeftSquareBracketRightSquareBracketKeyword_1_0());
+
+		List<Pair<Keyword,Keyword>> pairs = f.findKeywordPairs("{", "}");
+		for (Pair<Keyword, Keyword> pair : pairs) {
+			c.setIndentation(pair.getFirst(), pair.getSecond());
+			c.setLinewrap(2).after(pair.getSecond());
+		}
 	}
 }
