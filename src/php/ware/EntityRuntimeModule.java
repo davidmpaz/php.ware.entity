303f62bc3d10c3c73e4fc1a841bd07da13a35bdf
David Moises Paz Reyes <davidmpaz@gmail.com>
2011-04-03 14:01:13 -0400

    Set up and customize Services
    Add EntityValueConverterService and customize EntityScopeProvider,
    both ported from Domain model xtext example.

--- src/php/ware/EntityRuntimeModule.java
+++ src/php/ware/EntityRuntimeModule.java
@@ -3,9 +3,24 @@
  */
 package php.ware;
 
+import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.scoping.IScopeProvider;
+
+import php.ware.scoping.EntityScopeProvider;
+import php.ware.valueconverter.EntityValueConverterService;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class EntityRuntimeModule extends php.ware.AbstractEntityRuntimeModule {
 
+	@Override
+	public Class<? extends IScopeProvider> bindIScopeProvider() {
+		return EntityScopeProvider.class;
+	}
+
+	@Override
+	public Class<? extends IValueConverterService> bindIValueConverterService() {
+		return EntityValueConverterService.class;
+	}
 }
--- src/php/ware/scoping/EntityScopeProvider.java
+++ src/php/ware/scoping/EntityScopeProvider.java
@@ -3,15 +3,37 @@
  */
 package php.ware.scoping;
 
+import static org.eclipse.xtext.scoping.Scopes.scopeFor;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+
+import php.ware.entity.Entity;
+import php.ware.entity.Feature;
+import php.ware.entity.Reference;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 /**
  * This class contains custom scoping description.
  * 
  * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#scoping
- * on how and when to use it 
+ * on how and when to use it
  *
  */
 public class EntityScopeProvider extends AbstractDeclarativeScopeProvider {
 
+	public IScope scope_Reference_opposite(final Reference ref, EReference eRef) {
+		EList<Feature> features = ((Entity) ref.getType().getReferenced()).getFeatures();
+		Iterable<Reference> references = Iterables.filter(features, Reference.class);
+		references = Iterables.filter(references, new Predicate<Reference>(){
+			@Override
+			public boolean apply(Reference input) {
+				return ref.eContainer().equals(input.getType().getReferenced());
+			}});
+		return scopeFor(references);
+	}
 }
